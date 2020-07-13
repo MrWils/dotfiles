e917cf9270654e4bb2036722debed68d78de27e5
mrwils@tutanota.com <mrwils@tutanota.com>
2020-07-13 21:29:15 +0200

    Updated dotfiles, again

--- .zshrc
+++ .zshrc
@@ -71,17 +71,17 @@ export PATH="/home/rmw/Documents/Projects/GitProjects/hugo-source-pages/hugo_bui
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/opt/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/opt/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/opt/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
+# __conda_setup="$('/opt/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+# if [ $? -eq 0 ]; then
+#     eval "$__conda_setup"
+# else
+#     if [ -f "/opt/miniconda3/etc/profile.d/conda.sh" ]; then
+#         . "/opt/miniconda3/etc/profile.d/conda.sh"
+#     else
+#         export PATH="/opt/miniconda3/bin:$PATH"
+#     fi
+# fi
+# unset __conda_setup
 # <<< conda initialize <<<
 
-conda deactivate
+# conda deactivate
--- Documents/BashScripts/configure_pen_tablet.sh
+++ Documents/BashScripts/configure_pen_tablet.sh
@@ -1,20 +1,18 @@
 if xinput | grep -q "HID 256c:006d Pad pad"; then
-    # Pen Tablet, at home place
-    if xrandr | grep -q "DP2-2"; then
-        # Set display
-        xsetwacom set "HID 256c:006d Pen stylus" MapToOutput DP2-2
+    # Set display
+    xsetwacom set "HID 256c:006d Pen stylus" MapToOutput HEAD-0
+    # head is for nvidia drivers, DP2-2 or others can be used with regular drivers
 
-        # Button 1 is named 1
-        # Button 2 is named 2
-        # Button 3 is named 3
-        # Button 4 is named 8
-        # Button 5 is named 9
-        xsetwacom set "HID 256c:006d Pad pad" button 9 key B
-        # Button 6 is named 10
-        xsetwacom set "HID 256c:006d Pad pad" button 10 key Ctrl T
-        # Button 7 is named 11
-        xsetwacom set "HID 256c:006d Pad pad" button 11 key Ctrl
-        # Button 8 is named 12
-        xsetwacom set "HID 256c:006d Pad pad" button 12 key Ctrl Z
-    fi
+    # Button 1 is named 1
+    # Button 2 is named 2
+    # Button 3 is named 3
+    # Button 4 is named 8
+    # Button 5 is named 9
+    xsetwacom set "HID 256c:006d Pad pad" button 9 key B
+    # Button 6 is named 10
+    xsetwacom set "HID 256c:006d Pad pad" button 10 key Ctrl T
+    # Button 7 is named 11
+    xsetwacom set "HID 256c:006d Pad pad" button 11 key Ctrl
+    # Button 8 is named 12
+    xsetwacom set "HID 256c:006d Pad pad" button 12 key Ctrl Z
 fi
